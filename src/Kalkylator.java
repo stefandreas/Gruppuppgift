b14fd6dbfe982043f4804a315fbc7be65098c7f4
Stefandreas <stefan.johansson@edu.newton.se>
2021-09-20 10:39:31 +0200

    Färdigt

--- src/Kalkylator.java
+++ src/Kalkylator.java
@@ -6,8 +6,13 @@ public class Kalkylator {
         printMenu();
     }
 
-    /* printMenu skriver ut en meny med 5 val + 1 uppmaning. Användaren uppmanas att skriva in ett heltal mellan 1 0ch 5. Sen
-     * switchar vi på det heltalet */
+    /* Deltagare: Stefan Johansson och Emily Arnelid
+    Vi gör en string array och initierar den med 6 olika strängar.
+    printMenu använder en for-loop för att skriva ut en meny med 5 val + 1 uppmaning. Vi har en while-loop som använder
+    scan-objektet för att se om vi har input från användaren. If satsen kollar om det är ett heltal och om det är det,
+    så switchar vi på det värdet. Om värdet är utanför så körs default. Om inte ett heltal anges så körs else, där vi
+    nollställer scan-objektet och återvänder till while-loopen.
+    */
     public static void printMenu() {
         Scanner scan = new Scanner(System.in);
         String[] meny = {"1. Roten ur", "2. Procent", "3. Phytagoras sats", "4. Cirkelns area", "5. Avsluta",
@@ -21,7 +26,6 @@ public class Kalkylator {
         }
         while (scan.hasNext()) {
             if (scan.hasNextInt()) {
-
                 int choice = scan.nextInt();
                 switch (choice) {
                     case 1:
@@ -44,24 +48,32 @@ public class Kalkylator {
                         printMenu();
                         break;
                     case 4:
-                        System.out.println("Cirkelns area");
+                        System.out.println("Ange cirkelns diameter");
+                        double area = circleA();
+                        System.out.println("Cirkelns area är: "+ area);
+                        System.out.println();
+                        printMenu();
                         break;
                     case 5:
                         System.exit(0);
                         break;
                     default:
-                        System.out.println("Gör ett val!");
+                        System.out.print("Du måste ange ett nummer från menyn, försök igen : ");
                 }
+            }else {
+                scan.nextLine();
+                System.out.print("Du måste ange ett heltal, försök igen : ");
             }
         }
     }
 
-    /* Vi ber användaren om ett heltal mellan min och max, metoden tar emot två parametrar som begränsar storleken på talet.
-     Vi kontrollerar att det är ett heltal. Sedan returnerar vi heltalet. */
+    /* Vi ber användaren om ett heltal mellan min och max, metoden tar emot två parametrar som begränsar storleken på
+    talet. Vi använder en while loop som loopar tills vi fått ett korrekt värde.
+    Vi använder en boolean för att stoppa loopen när vi har ett godkänt värde. Sedan returnerar vi heltalet. */
     public static int getVal(int min, int max) {
         int heltal = 0;
         int retval = 0;
-        System.out.println("Ange ett heltal mellan " + min + " och " + max + " : ");
+        System.out.print("Ange ett heltal mellan " + min + " och " + max + " : ");
         Scanner scan2 = new Scanner(System.in);
         boolean vimpel = true;
         while (vimpel) {
@@ -72,23 +84,22 @@ public class Kalkylator {
                     vimpel = false;
                 } else {
                     scan2.nextLine();
-                    System.out.println("Det var inte ett heltal mellan " + min + " och " + max + " , försök igen : " );
+                    System.out.print("Det var inte ett heltal mellan " + min + " och " + max + " , försök igen : " );
                 }
             } else {
                 scan2.nextLine();
-                System.out.println("Det var inte ett heltal, försök igen : ");
+                System.out.print("Det var inte ett heltal, försök igen : ");
             }
         }
-
         return retval;
     }
     /*Vi ber användaren om ett flyttal mellan min och max, metoden tar emot två parametrar som begränsar storleken på
-    * talet. Vi Kontrollerar att det är ett flyttal. Sedan returnera vi flyttalet. Vi använder en while loop som loopar tills
-    * vi fått ett korrekt värde.*/
+    * talet. Vi Kontrollerar att det är ett flyttal. Sedan returnera vi flyttalet. Vi använder en while loop som loopar
+    * tills vi fått ett korrekt värde. Vi använder en boolean för att stoppa loopen när vi har ett godkänt värde.*/
     public static double getVal(double min, double max){
         double flyttal = 0;
         double returnValue = 0;
-        System.out.println("Ange ett flyttal mellan " + min + " och " + max + " : ");
+        System.out.print("Ange ett flyttal mellan " + min + " och " + max + " : ");
         Scanner scan5 = new Scanner(System.in);
         boolean flagga = true;
         while (flagga) {
@@ -99,18 +110,18 @@ public class Kalkylator {
                     flagga = false;
                 } else {
                     scan5.nextLine();
-                    System.out.println("Det var inte inom gränserna, försök igen : ");
+                    System.out.print("Det var inte inom gränserna, försök igen : ");
                 }
             } else {
                 scan5.nextLine();
-                System.out.println("Det här var inte ett flyttal, försök igen : ");
+                System.out.print("Det här var inte ett flyttal, försök igen : ");
             }
         }
         return returnValue;
         }
 
-    /*Vi anropar getVal med min och max och castar svaret till en double variabel som vi skickar in som argument till Math.sqrt
-    * och returnerar double  */
+    /*Vi anropar getVal med min och max och castar svaret till en double variabel som vi skickar in som argument till
+    Math.sqrt och returnerar värdet  */
     public static double sqrtof() {
         int min = 0;
         int max = 1000;
@@ -121,7 +132,9 @@ public class Kalkylator {
     }
     /*Vi kom på efter att vi gjort metoden att vi inte hade behövt göra en specialanpassad metod för procent.
     * double getVal finns högre upp
-    * Vi skapade en array så att vi kunde stoppa in två värden i den för att returnera till procentmetoden */
+    * Vi skapade en array så att vi kunde stoppa in två värden i den för att returnera till procentmetoden
+    * Så den här metoden är lite onödig men vi lät den vara kvar som ett exempel på när man inte har läst i
+    * uppgiften ordentligt. */
     public static double[] getVal(double minPro, double maxPro, double minVal, double maxVal) {
         double heltal = 0;
         double[] arrayReturn = new double[2];
@@ -138,11 +151,11 @@ public class Kalkylator {
                     flag = false;
                 } else {
                     scan3.nextLine();
-                    System.out.println("Det var inte ett procenttal mellan 0 och 100, försök igen :");
+                    System.out.print("Det var inte ett procenttal mellan 0 och 100, försök igen :");
                 }
             } else {
                 scan3.nextLine();
-                System.out.println("Det var inte ett procenttal mellan 0 och 100, försök igen :");
+                System.out.print("Det var inte ett procenttal mellan 0 och 100, försök igen :");
             }
         }
         while (flag2) {
@@ -155,11 +168,11 @@ public class Kalkylator {
                     flag2 = false;
                 }else {
                     scan4.nextLine();
-                    System.out.println("Det var inte ett tal mellan 1 och 50, försök igen :");
+                    System.out.print("Det var inte ett tal mellan 1 och 50, försök igen :");
                 }
             } else {
                 scan4.nextLine();
-                System.out.println("Det var inte ett tal mellan 1 och 50, försök igen :");
+                System.out.print("Det var inte ett tal mellan 1 och 50, försök igen :");
             }
         }
                 return arrayReturn;
@@ -180,19 +193,28 @@ public class Kalkylator {
         double andel = procent/decimal * value;
         return andel;
     }
-    /*Vi anropar double getVal som returnerar två stycken katedrar. Som vi använder i vårat anrop Math.hypot. Sedan returneras
-    * hypotenusan.*/
+    /*Vi anropar double getVal som returnerar två stycken katedrar. Som vi använder i vårat anrop Math.hypot. Sedan
+    returneras hypotenusan.*/
     public static double pythagoras(){
         double min = 1;
         double max = 1000;
         double kateder1 = 0;
         double kateder2 = 0;
         kateder1 = getVal(min, max);
+        System.out.println("Kateder nummer 2 ");
         kateder2 = getVal(min, max);
         double hypotenusa = Math.hypot(kateder1, kateder2);
         return hypotenusa;
     }
-
+    /* Anropar double getVal och frågar om en diameter. Sedan dividerar vi diametern med 2 för att få fram radien,
+    sedan gör vi bara uträkningen för area och returnerar värdet.  */
+    public static double circleA(){
+        double diameter = 0;
+        diameter = getVal(1, 1000);
+        double radie = diameter/2;
+        double area = Math.PI * radie * radie;
+        return area;
+    }
 }
 
 
